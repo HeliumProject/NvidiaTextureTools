89016e979a3c18ab58498271110db0dd9fde9264
Geoff Evans <geoff@flummoxed.org>
2017-10-22 11:32:29 -0700

    fix for macosx 10.13

--- project/macosx/nvconfig.h
+++ project/macosx/nvconfig.h
@@ -1,14 +1,14 @@
 #ifndef NV_CONFIG
 #define NV_CONFIG
 
-#define HAVE_UNISTD_H
-#define HAVE_STDARG_H
-#define HAVE_SIGNAL_H
-#define HAVE_EXECINFO_H
+#define HAVE_UNISTD_H 1
+#define HAVE_STDARG_H 1
+#define HAVE_SIGNAL_H 1
+#define HAVE_EXECINFO_H 1
 /* #undef HAVE_MALLOC_H */
 
 /* #undef HAVE_OPENMP */
-#define HAVE_DISPATCH_H
+#define HAVE_DISPATCH_H 1
 
 /*#define HAVE_STBIMAGE*/
 /* #undef HAVE_PNG */
